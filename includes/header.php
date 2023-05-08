20784b773a5d4f0f31c189b961a4a889af30cd4a
Amil Silahic <jinobie02@gmail.com>
2023-05-07 22:17:52 -0600

    adding css folder and testing it for now - might use something like bootstrap to make my life easier

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
 <head>
     <title>Dev Log Posts</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
-echo "Hello from the server";
+// have to use these since my ini is messed up
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 ?>
 
 <?php require "includes/header.php" ?>
-<ul>
-    <li>
-        List 1
-    </li>
-</ul>
+<div>
+    <article>
+        <h2>This is the title</h2>
+        <p>This is the content</p>
+    </article>
+</div>
 <?php require "includes/footer.php" ?>
