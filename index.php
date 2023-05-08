1d3e80f28bf85d193ed915f6a828de046a6812bb
Amil Silahic <jinobie02@gmail.com>
2023-05-08 14:14:40 -0600

    found vscode extension for formatting

--- index.php
+++ index.php
@@ -7,10 +7,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-
 $sql = "SELECT * FROM logs ORDER BY published_at";
 
 $query = $conn->query($sql);
+
+
+
+
 
 $results = $query->fetchAll(PDO::FETCH_ASSOC);
 
@@ -26,7 +29,6 @@ var_dump($results);
             <h2><?= $article["title"] ?></h2>
             <span><?= $article["published_at"] ?></span>
             <p><?= $article["content"] ?></p>
-
         </article>
     <?php endforeach; ?>
 </div>
