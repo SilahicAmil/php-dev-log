cf6cb579de6cb6ce829752ba91d9b2503ef1a484
Amil Silahic <jinobie02@gmail.com>
2023-05-08 13:23:49 -0600

    adding styling to dev post

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $conn = require "includes/db.php";
+
 // have to use these since my ini is messed up
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -19,12 +20,15 @@ var_dump($results);
 ?>
 
 <?php require "includes/header.php" ?>
-<div></div>
-<?php foreach ($results as $article) : ?>
-    <article>
-        <h2><?= $article["title"] ?></h2>
-        <p><?= $article["content"] ?></p>
-    </article>
-<?php endforeach; ?>
+<div id="article-container">
+    <?php foreach ($results as $article) : ?>
+        <article>
+            <h2><?= $article["title"] ?></h2>
+            <span><?= $article["published_at"] ?></span>
+            <p><?= $article["content"] ?></p>
+
+        </article>
+    <?php endforeach; ?>
+</div>
 </div>
 <?php require "includes/footer.php" ?>
