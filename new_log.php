1e296706cac951f3dac6d0612a97d5e6ad76dffb
Amil Silahic <jinobie02@gmail.com>
2023-05-09 20:49:45 -0600

    misc

--- new_log.php
+++ new_log.php
@@ -28,25 +28,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <?php require "includes/header.php" ?>
 
 <form method="POST">
-    <div class="d-flex-col m-auto w-75">
+    <div class="d-flex-col m-auto w-75 mt-5">
         <div class="mb-3">
             <label for="title" class="fs-5">Title</label>
             <!-- name is key here for getting value (equivalent to ref={} in react) -->
-            <input type="text" name="title" id="title" placeholder="Article Title"
-                value="<?= htmlspecialchars($article->title ?? "") ?>" class="form-control">
+            <input type="text" name="title" id="title" placeholder="Article Title" value="<?= htmlspecialchars($article->title ?? "") ?>" class="form-control">
         </div>
         <div class="mb-3">
             <label for="content" class="fs-5">Content</label>
-            <textarea name="content" id="content" cols="30" rows="10" placeholder="Article Content"
-                class="form-control"><?= htmlspecialchars($article->content ?? "") ?></textarea>
+            <textarea name="content" id="content" cols="30" rows="10" placeholder="Article Content" class="form-control"><?= htmlspecialchars($article->content ?? "") ?></textarea>
         </div>
         <div class="mb-3">
             <label for="published_at" class="fs-5">Published At</label>
-            <input type="datetime-local" name="published_at" id="published_at"
-                value="<?= htmlspecialchars($article->published_at ?? "") ?>" class="form-control">
+            <input type="datetime-local" name="published_at" id="published_at" value="<?= htmlspecialchars($article->published_at ?? "") ?>" class="form-control">
         </div>
-        <div class="d-flex justify-content-end"> <button type="submit"
-                class="btn btn-success w-25 p-3 fs-5">Create</button>
+        <div class="d-flex justify-content-end"> <button type="submit" class="btn btn-success w-25 p-3 fs-5">Create</button>
         </div>
     </div>
 </form>
