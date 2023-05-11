90245b8e58129e2f16f0b5feaedae8e63a436acb
Amil Silahic <jinobie02@gmail.com>
2023-05-11 17:57:07 -0600

    removing css sicne im using bootstrap in this project

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,6 @@
 <head>
     <title>Dev Log Posts</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
-    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"
