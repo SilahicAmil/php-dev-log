f853e0feba4d93bc0a4864fb2fe30911e774f79f
Amil Silahic <jinobie02@gmail.com>
2023-05-11 18:23:22 -0600

    updating jquery to change text on the button to me Show More or Show Less depending on the toggle state

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ $(document).ready(function () {
   // Toggle Placeholder Content on ?id=:id page
   $("#show-more-text").hide();
   $("#show-more-content").click(function () {
+    $(this).text(function (i, text) {
+      // if text is show more then when clicked set to show less
+      // else when "unclicked" show more
+      return text === "Show More" ? "Show Less" : "Show More";
+    });
     $("#show-more-text").toggle();
   });
 });
