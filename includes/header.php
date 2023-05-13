12e314fd152adcbbd899b20bb549befe6084446e
Amil Silahic <jinobie02@gmail.com>
2023-05-13 12:50:35 -0600

    echoing $_POST values from signup form for now - now creating DB

--- includes/header.php
+++ includes/header.php
@@ -29,7 +29,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="new_log.php">new log article</a>
                     </li>
-
+                    <li class="nav-item">
+                        <a class="nav-link" href="signup.php">signup</a>
+                    </li>
                 </ul>
             </div>
         </div>
--- signup.php
+++ signup.php
@@ -1,27 +1,37 @@
 <?php
 
+// have to use these since my ini is messed up
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo $_POST["username"];
+    echo $_POST["email"];
+    echo $_POST["password"];
+}
 ?>
 
-<?php require "includes/header.php" ?>
+<?php require "includes/header.php"; ?>
 
 <form method="POST">
 
     <div>
         <label for="username">Username</label>
-        <input type="text" id="username">
+        <input type="text" id="username" name="username">
     </div>
 
     <div>
         <label for="email">Email</label>
-        <input type="email" id="email">
+        <input type="email" id="email" name="email">
     </div>
 
     <div>
         <label for="password">Password</label>
-        <input type="passowrd" id="password">
+        <input type="passowrd" id="password" name="password">
     </div>
 
     <button>Sign Up</button>
 
 </form>
-<?php require "includes/footer.php" ?>
+<?php require "includes/footer.php"; ?>
