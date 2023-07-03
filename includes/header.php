84c12aa669c65e040fe8983c273dff2a9b35802b
Amil Silahic <jinobie02@gmail.com>
2023-07-03 15:53:21 -0600

    comments

--- includes/header.php
+++ includes/header.php
@@ -4,12 +4,13 @@
 <head>
     <title>Dev Log Posts</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
-
+    <!-- boostrap css -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- jQUERY -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"
         integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-
+    <!-- bootstrap script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
         integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
     </script>
